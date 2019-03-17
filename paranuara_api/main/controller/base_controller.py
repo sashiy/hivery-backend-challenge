8350360899236d8747fae58bb86beb2bd78e24f5
Sasikanth Yalamarthy <sasikanth.yalamarthy@gmail.com>
2019-03-17 23:20:21 +1100

    Adding comments

--- paranuara_api/main/controller/base_controller.py
+++ paranuara_api/main/controller/base_controller.py
@@ -3,6 +3,11 @@ from paranuara_api.main.utilities.utilities import logging
 
 class BaseController:
     def _create_dict_for_response(self, person_details):
+        """
+        This method creates and returns dictionary of person with limited and trivial information
+        :param person_details: Person JSON with all relevant information
+        :return: Person dictionary with trivial infroamtion
+        """
         try:
             person_dict = dict()
             person_dict['username'] = person_details['name']
