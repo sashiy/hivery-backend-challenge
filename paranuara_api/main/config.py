e4c7740bd031a60aad02394dfbe63425c6053691
Sasikanth Yalamarthy <sasikanth.yalamarthy@gmail.com>
2019-03-17 23:26:41 +1100

    Adding comments

--- paranuara_api/main/config.py
+++ paranuara_api/main/config.py
@@ -4,9 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    """
+    This class can hold all the configurations related to application.
+    """
     DEBUG = True
     COMPANIES_JSON = os.path.join(basedir, 'resources', 'companies.json')
     PEOPLE_JSON = os.path.join(basedir, 'resources', 'people.json')
     FRUITS_JSON = os.path.join(basedir, 'resources', 'fruits.json')
     VEGETABLES_JSON = os.path.join(basedir, 'resources', 'vegetables.json')
-    LOG_FILE_PATH= os.path.join(basedir, 'logs', 'paranuara_api.log')
+    LOG_FILE_PATH = os.path.join(basedir, 'logs', 'paranuara_api.log')
